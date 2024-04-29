92d8c7bbaf62f577564f0e250f7e2de900095b29
rlneto <rlneto@live.com>
2024-04-29 01:58:20 -0300

    Navbar Pronta

---
+++ app/carreira/page.tsx
@@ -0,0 +1,7 @@
+export default function Carreira() {
+  return(
+    <div>
+      Carreira
+    </div>
+  )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+export default function Navbar() {
+  return (
+    <div className="flex w-screen h-20 bg-[#023047]"></div>
+  );
+}
