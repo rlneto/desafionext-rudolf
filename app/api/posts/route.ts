65557c5c08e5fc6f6402137acfd9d1d52bcb3844
Rudolfo Lange Neto <rlneto@live.com>
2024-07-30 06:58:53 -0300

    Route prism added

---
+++ app/api/posts/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '../../../lib/prisma';
+
+export async function GET() {
+  try {
+    const posts = await prisma.post.findMany();
+    return NextResponse.json(posts);
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
